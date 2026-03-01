b463d83167cb5a81bc973f01447fe564a4e68a7d
Lancer385 <ahmedkhalaf1234123@gmail.com>
2026-03-01 04:06:35 +0200

    test: add test for reaching stock capacity - divide increment and decrement tests since they test 2 behaviors

--- src/tests/components/item.test.jsx
+++ src/tests/components/item.test.jsx
@@ -14,7 +14,7 @@ const product = {
 }
 
 const cartItems = {
-    9: {count : 99}
+    1: {count : 48}
 }
 
 
@@ -69,7 +69,23 @@ describe( 'Shop Item', () =>{
         await user.type(input, '999') // the input will auto-cap based on stock value.
         expect(input).toHaveValue(parseInt(product.stock));  
     })
-    it("shouldn't decrement if the input value is 0 and shouldn't increment if the value is maximum", async () => {
+    it("shouldn't decrement if the input value is 0", async () => {
+        const user = userEvent.setup();
+        const handleCartChanges = vi.fn();
+        render(<Item
+            value={product}            
+            handleCartChanges={handleCartChanges}
+            initCount={0} // default value of the input
+            cartItems={cartItems}
+            isCartItem={false}
+        />) 
+        const input = screen.getByRole('spinbutton')
+        const decrement = screen.getByRole("button", { name: '-' });
+        await user.click(decrement); // button should be disabled
+        expect(input).toHaveValue(0);
+    })
+
+    it("shouldn't increment if the value hit the max stock", async () => {
         const user = userEvent.setup();
         const handleCartChanges = vi.fn();
         render(<Item
@@ -81,21 +97,19 @@ describe( 'Shop Item', () =>{
         />) 
         const input = screen.getByRole('spinbutton')
         const increment = screen.getByRole("button", {name: '+'});
-        const decrement = screen.getByRole("button", { name: '-' });
-        await user.click(decrement); // button should be disabled
-        expect(input).toHaveValue(0);
         await user.type(input, "99") // this will auto-cap, based on our third test!
         await user.click(increment) // thus this will be disabled
         expect(input).toHaveValue(parseInt(product.stock));
 
     })
 
-    it("should itself if it exists inside cart component", async () =>{
+
+    it("should delete itself if it exists inside cart component", async () =>{
         const user = userEvent.setup();
         const handleCartChanges = vi.fn();
         render(<Item
             value={product}            
-            handleCartChanges={handleCartChanges} // this function is different between shop and cart component
+            handleCartChanges={handleCartChanges} // this function is handled differently between shop and cart component
             initCount={0}
             cartItems={cartItems}
             isCartItem={true} // this tells the component that it lives inside the cart
@@ -104,5 +118,26 @@ describe( 'Shop Item', () =>{
         await user.click(remove)
         expect(handleCartChanges).toHaveBeenCalled();
     })
-}
-)
+    it("shouldn't add items to cart if the cart already reached the max stock of said item", async () => {
+        const user = userEvent.setup();
+        const handleCartChanges = vi.fn();
+        render(<Item
+            value={product}            
+            handleCartChanges={handleCartChanges}
+            initCount={0}
+            cartItems={cartItems}
+            isCartItem={false}
+        />) 
+        const input = screen.getByRole('spinbutton')
+        const increment = screen.getByRole("button", {name: '+'});
+        const addToCart = screen.getByRole("button", { name: "Add to Cart" });
+        // so the cart already has 48 items
+        await user.click(increment) // should be fine to increment = 1
+        await user.click(increment) // and again = 2
+        await user.click(increment) // shouldn't be called here since the max stock is 50
+        expect(input).toHaveValue(2);
+        await user.click(addToCart)
+        expect(handleCartChanges).toBeCalled()
+        // now both increment and add to cart buttons are disabled 
+    })
+})
