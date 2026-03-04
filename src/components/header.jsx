600718a3deabd8b8585a4ccf5c6e0a1ef1dc7283
Lancer385 <ahmedkhalaf1234123@gmail.com>
2026-03-04 06:34:28 +0200

    feat: add cart items counter - add various class names for styling - add cart icon, replacing "cart" text - add lucide-react as dependency for icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import Styles from "../styles/header.module.css"
+import { ShoppingCart } from "lucide-react";
 
-function Header() {
+function Header({cartItems}) {
+  const totalItems = Object.values(cartItems).reduce((sum, value) => sum + value.count, 0);
+  const cartDisplay = totalItems === 0 ? null : totalItems > 9 ? '9+' : `${totalItems}`;
   return (
-    <header>
-      <nav>
-        <Link to="home">Home</Link>
-        <Link to="shop">Shop</Link>
-        <Link to="cart">Cart</Link>
+    <header className={Styles.header}>
+      <h1 className={Styles.heading}>Shopping Cart</h1>
+      <nav className={Styles.navigation}>
+        <Link className={Styles.navOne} to="home">Home</Link>
+        <Link className={Styles.navTwo} to="shop">Shop</Link>
+        <Link className={Styles.navThree}to="cart" aria-label="Cart"> <ShoppingCart width={32} height={32} /><span className={Styles.number}>{cartDisplay}</span></Link>
       </nav>
     </header>
   );
